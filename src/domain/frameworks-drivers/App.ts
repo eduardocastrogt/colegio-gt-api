f8b50245b0d5da734630dfc0de5fa2cde5f916f6
Eduardo Castro <eduardocastrogt@gmail.com>
2024-06-22 12:32:29 -0600

    Agregado swagger al levantado del api

--- src/domain/frameworks-drivers/App.ts
+++ src/domain/frameworks-drivers/App.ts
@@ -1,16 +1,39 @@
 import express, { Application, Request, Response } from 'express'
 import helmet from 'helmet'
 import dotenv from 'dotenv'
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+
+import generoRouter from '../routes/generoRoutes'
 dotenv.config()
 
 export class App {
   private readonly port: string = process.env.PORT ?? '3000'
+  private readonly version: string = process.env.VERSION ?? 'v1'
+  private readonly prefixApi: string = process.env.PREFIX_API ?? '/api'
   public app: Application
 
   constructor () {
     this.app = express()
     this.middlewares()
     this.routes()
+    this.swagger()
+  }
+
+  private swagger (): void {
+    const options = {
+      definition: {
+        openapi: '3.0.0',
+        info: {
+          title: 'API Documentation',
+          version: '1.0.0'
+        }
+      },
+      apis: ['./src/domain/routes/*.ts']
+    }
+
+    const swaggerSpec = swaggerJsdoc(options)
+    this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
   }
 
   private middlewares (): void {
@@ -24,6 +47,9 @@ export class App {
     this.app.get('/', (_req: Request, res: Response) => {
       res.send('Hello World')
     })
+
+    // Obtener generos
+    this.app.use(`/${this.version}/${this.prefixApi}`, generoRouter)
   }
 
   async start (): Promise<void> {
