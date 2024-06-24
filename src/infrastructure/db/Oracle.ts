ebe32b02de6eb339c3b49bc10342a77a98b6fcc5
Eduardo Castro <eduardocastrogt@gmail.com>
2024-06-23 20:23:37 -0600

    Actualizada conexión a oracle

--- src/infrastructure/db/Oracle.ts
+++ src/infrastructure/db/Oracle.ts
@@ -3,82 +3,104 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 class Oracle {
-  private connection: any
+  private pool: any
 
   constructor () {
-    oracledb.createPool({
-      user: process.env.ORACLE_USER ?? '',
-      password: process.env.ORACLE_PASSWORD ?? '',
-      connectString: process.env.ORACLE_HOST ?? '',
-      poolMax: 10,
-      poolMin: 2,
-      poolTimeout: 60,
-      poolIncrement: 2
+    this.initializePool().then(() => {
+      console.log('Pool created successfully')
+    }).catch((err) => {
+      console.error('Error creating pool:', err)
     })
-      .then(() => {
-        console.log('Pool created successfully')
-      })
-      .catch((err) => {
-        console.error('Error creating pool:', err)
-      })
   }
 
-  async obtenerConexion (): Promise<any> {
+  private async initializePool (): Promise<void> {
     try {
-      const connection = await oracledb.getConnection()
-      return connection
+      this.pool = await oracledb.createPool({
+        user: process.env.ORACLE_USER ?? '',
+        password: process.env.ORACLE_PASSWORD ?? '',
+        connectString: process.env.ORACLE_HOST ?? '',
+        poolMax: 10,
+        poolMin: 2,
+        poolTimeout: 60,
+        poolIncrement: 2
+      })
+      console.log('Pool created successfully')
     } catch (err) {
-      console.error('getConnection() error: ' + err)
+      console.error('Error creating pool:', err)
       throw err
     }
   }
 
   async select (query: string): Promise<{ status: string, message: string, data?: any[] }> {
+    let connection: any
     try {
-      this.connection = await oracledb.getConnection()
-      const result = await this.connection.execute(query)
-      this.connection.close()
-      const data = result.rows
-
-      return { status: 'success', message: 'Consulta exitosa', data }
+      if (this.pool != null) {
+        connection = await this.pool.getConnection()
+        const result = await connection.execute(query)
+        return { status: 'success', message: 'Consulta exitosa', data: result.rows }
+      } else {
+        return { status: 'error', message: 'Error al ejecutar consulta SELECT' }
+      }
     } catch (err) {
       console.error('Error al ejecutar consulta SELECT:', err)
       return { status: 'error', message: 'Error al ejecutar consulta SELECT' }
+    } finally {
+      if (connection != null) {
+        try {
+          await connection.close()
+        } catch (err) {
+          console.error('Error al cerrar conexión:', err)
+        }
+      }
     }
   }
 
   async execute (query: string, params: any): Promise<{ status: string, message: string, rowsAffected?: number, data?: any }> {
+    let connection
     try {
-      this.connection = await this.obtenerConexion()
-
-      const result = await this.connection.execute(query, params, { autoCommit: true })
-      const rowsAffected = result.rowsAffected
-      await this.connection.close()
-
-      return { status: 'success', message: 'Query ejecutado exitosamente', rowsAffected, data: result.outBinds }
+      connection = await this.pool.getConnection()
+      const result = await connection.execute(query, params, { autoCommit: true })
+      return { status: 'success', message: 'Query ejecutado exitosamente', rowsAffected: result.rowsAffected, data: result.outBinds }
     } catch (err) {
       console.error('Error al ejecutar query:', err)
       return { status: 'error', message: 'Error al ejecutar query' }
+    } finally {
+      if (connection != null) {
+        try {
+          await connection.close()
+        } catch (err) {
+          console.error('Error al cerrar conexión:', err)
+        }
+      }
     }
   }
 
   async executeProcedure (procedureName: string, params: any): Promise<{ status: string, message: string }> {
+    let connection
     try {
-      await this.connection.execute(procedureName, params)
-
+      connection = await this.pool.getConnection()
+      await connection.execute(procedureName, params)
       return { status: 'success', message: 'Procedimiento almacenado ejecutado exitosamente' }
     } catch (err) {
       console.error('Error al ejecutar procedimiento almacenado:', err)
       return { status: 'error', message: 'Error al ejecutar procedimiento almacenado' }
+    } finally {
+      if (connection != null) {
+        try {
+          await connection.close()
+        } catch (err) {
+          console.error('Error al cerrar conexión:', err)
+        }
+      }
     }
   }
 
-  async closeConnection (): Promise<void> {
+  async closePool (): Promise<void> {
     try {
-      await this.connection.close()
-      console.log('Conexión cerrada')
+      await this.pool.close()
+      console.log('Pool cerrado')
     } catch (err) {
-      console.error('Error al cerrar la conexión:', err)
+      console.error('Error al cerrar pool:', err)
     }
   }
 }
